2c19e53e017a4b5c70863993b44e5f058a4af571
Farhad Esmaeili <farhadesmzdh@gmail.com>
2024-07-26 23:38:52 +0330

    chore: refactor setup initialization in tests

--- library/src/test/classes/LibraryTest.java
+++ library/src/test/classes/LibraryTest.java
@@ -2,27 +2,36 @@ package test.classes;
 
 import main.classes.Library;
 import main.classes.SearchByType;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class LibraryTest {
+    private Library library;
+
+    @BeforeEach
+    public void setUp() {
+        library = new Library();
+    }
+
+    private ArrayList<Object> createKeys(Object... keys) {
+        return new ArrayList<>(List.of(keys));
+    }
+
     @Test
     @DisplayName("searchBooks must return null when no book is found")
     public void searchBooksNoBookFound() {
-        Library library = new Library();
-        ArrayList<Object> keys = new ArrayList<>(List.of(1));
+        ArrayList<Object> keys = createKeys(1);
         Assertions.assertNull(library.searchBooks(SearchByType.ID, keys));
     }
 
     @Test
     @DisplayName("searchBooks must throw an exception when search is by name")
     public void searchBooksSearchByName() {
-        Library library = new Library();
         ArrayList<Object> keys = new ArrayList<>(List.of("Boostan"));
         Assertions.assertThrows(IllegalArgumentException.class, () -> library.searchBooks(SearchByType.NAME, keys));
     }
