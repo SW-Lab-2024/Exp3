a0011a8e927d602cd3af87e366495d1f21da9d77
Farhad Esmaeili <farhadesmzdh@gmail.com>
2024-07-26 23:24:09 +0330

    feat: add test for searchbooks when name is illegal type

--- library/src/test/classes/LibraryTest.java
+++ library/src/test/classes/LibraryTest.java
@@ -12,11 +12,18 @@ import java.util.List;
 
 public class LibraryTest {
     @Test
-    @DisplayName("No book found in searching books")
+    @DisplayName("searchBooks must return null when no book is found")
     public void searchBooksNoBookFound() {
         Library library = new Library();
         ArrayList<Object> keys = new ArrayList<>(List.of(1));
         Assertions.assertNull(library.searchBooks(SearchByType.ID, keys));
     }
 
+    @Test
+    @DisplayName("searchBooks must throw an exception when search is by name")
+    public void searchBooksSearchByName() {
+        Library library = new Library();
+        ArrayList<Object> keys = new ArrayList<>(List.of("Boostan"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> library.searchBooks(SearchByType.NAME, keys));
+    }
 }
