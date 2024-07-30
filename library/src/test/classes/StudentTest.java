2d075297950a432642baed5703ed5518196e5bfd
ffugue <heliakaviansharif@gmail.com>
2024-07-31 00:08:28 +0330

    refactor the StudentTest tests and add setUp method

--- library/src/test/classes/StudentTest.java
+++ library/src/test/classes/StudentTest.java
@@ -1,15 +1,21 @@
 package classes;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StudentTest {
+    private Student student;
+
+    @BeforeEach
+    public void setUp() {
+        student = new Student("Name", 1);
+    }
 
     @Test
     @DisplayName("Test Student constructor and getters")
     public void testStudentConstructorAndGetters() {
-        Student student = new Student("Name", 1);
         Assertions.assertEquals("Name", student.getName());
         Assertions.assertEquals(1, student.getId());
     }
@@ -17,14 +23,12 @@ public class StudentTest {
     @Test
     @DisplayName("Test Student toString method")
     public void testToString() {
-        Student student = new Student("Name", 1);
         Assertions.assertEquals("Name|1", student.toString());
     }
 
     @Test
     @DisplayName("Test matches method with ID")
     public void testMatchesWithId() {
-        Student student = new Student("Name", 1);
         Assertions.assertTrue(student.matches(SearchByType.ID, 1));
         Assertions.assertFalse(student.matches(SearchByType.ID, 2));
     }
@@ -32,7 +36,6 @@ public class StudentTest {
     @Test
     @DisplayName("Test matches method with Name")
     public void testMatchesWithName() {
-        Student student = new Student("Name", 1);
         Assertions.assertTrue(student.matches(SearchByType.NAME, "Name"));
         Assertions.assertFalse(student.matches(SearchByType.NAME, "Other"));
     }
