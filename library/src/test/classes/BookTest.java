87f1a3a6d838371f8985760bc32dd32c8fb36f2a
ffugue <heliakaviansharif@gmail.com>
2024-07-30 23:59:12 +0330

    refactor the BookTest tests and add setUp method

--- library/src/test/classes/BookTest.java
+++ library/src/test/classes/BookTest.java
@@ -1,15 +1,21 @@
 package classes;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BookTest {
+    private Book book;
+
+    @BeforeEach
+    public void setUp() {
+        book = new Book("Title", "Author", 1);
+    }
 
     @Test
     @DisplayName("Test Book constructor and getters")
     public void testBookConstructorAndGetters() {
-        Book book = new Book("Title", "Author", 1);
         Assertions.assertEquals("Title", book.getTitle());
         Assertions.assertEquals("Author", book.getAuthor());
         Assertions.assertEquals(1, book.getId());
@@ -18,7 +24,6 @@ public class BookTest {
     @Test
     @DisplayName("Test Book toString method")
     public void testToString() {
-        Book book = new Book("Title", "Author", 1);
         Assertions.assertEquals("Title by Author", book.toString());
     }
 }
